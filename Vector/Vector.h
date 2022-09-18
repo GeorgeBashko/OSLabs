53c561679678c47359c74f63e688bc82619df706
GeorgeBashko <90065629+GeorgeBashko@users.noreply.github.com>
2022-09-18 22:52:38 +0300

    Add files via upload

---
+++ Vector/Vector.h
@@ -0,0 +1,22 @@
+#ifndef VECTOR_VECTOR_H
+#define VECTOR_VECTOR_H
+
+#include "../Number/Number.h"
+
+class Vector
+{
+private:
+    Number x;
+    Number y;
+public:
+    Vector(Number, Number);
+    void AddVector(Vector a, Vector b);
+    double angelCacl();
+    double radiusCalc();
+    Number getX() const;
+    Number getY() const;
+};
+const Vector ZERO = Vector(Number(0),Number(0));
+const Vector ONE = Vector(Number(1), Number(1));
+
+#endif //VECTOR_VECTOR_H
