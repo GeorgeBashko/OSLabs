b01f01023d44b8aa9d541b8a005c16cebd9c121c
GeorgeBashko <90065629+GeorgeBashko@users.noreply.github.com>
2022-09-18 22:51:16 +0300

    Add files via upload

---
+++ MainTest/cmake-build-debug/CMakeFiles/MainTest.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/geprge/CLionProjects/MainTest/main.cpp" "CMakeFiles/MainTest.dir/main.cpp.o" "gcc" "CMakeFiles/MainTest.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/geprge/CLionProjects/MainTest/cmake-build-debug/CMakeFiles/Number.dir/DependInfo.cmake"
+  "/home/geprge/CLionProjects/MainTest/cmake-build-debug/CMakeFiles/Vector.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
